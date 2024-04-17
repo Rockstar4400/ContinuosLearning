65fe4e501ad891cd12ddacb3bb80f05ccd51d78f
Alberto Del Angel <rockstar4400@gmail.com>
2024-04-17 09:06:42 -0600

    Java 22 - Example interfaces and classes

--- Master-Java/Practices/ReversedAPP/src/ParkRanger.java
+++ Master-Java/Practices/ReversedAPP/src/ParkRanger.java
@@ -1,2 +1,19 @@
-package PACKAGE_NAME;public class ParkRanger {
+interface WalksOn4Legs {
+}
+
+abstract class Mammal {
+    public int numberOfOffspring;
+}
+
+class Antelope extends Mammal implements WalksOn4Legs {
+}
+
+public class ParkRanger {
+    public void noteNewOffspring(Mammal mammal) {
+        mammal.numberOfOffspring++;
+    }
+
+    public static void main(String[] args) {
+        new ParkRanger().noteNewOffspring(null);
+    }
 }
