3ebd92e0d44cc51e9dfb22c7b7b65260ddcf1049
Alberto Del Angel <rockstar4400@gmail.com>
2024-05-14 21:01:54 -0600

    Exploring Layouts

--- MasterAndroid/Books/AndroidProgramming/01.ExploringLayouts/app/src/main/java/com/magicbytes/exploringlayouts/MainActivity.kt
+++ MasterAndroid/Books/AndroidProgramming/01.ExploringLayouts/app/src/main/java/com/magicbytes/exploringlayouts/MainActivity.kt
@@ -1,4 +1,25 @@
 package com.magicbytes.exploringlayouts
 
-class MainActivity {
+import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    fun loadConstraintLayout(v: View) {
+        setContentView(R.layout.activity_main)
+    }
+
+    fun loadTableLayout(v: View) {
+        setContentView(R.layout.my_table_layout)
+    }
+
+    fun loadMenuLayout(v: View) {
+        setContentView(R.layout.main_menu)
+    }
 }
