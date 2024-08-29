108cccec9930afd2a262311be37a2cf8b4032278
Alberto Del Angel <rockstar4400@gmail.com>
2024-08-29 15:53:37 -0600

    Master React - Retaking chapter 21

--- MasterReact/Udemy-ReactCourses/Udemy-React-Stephen-Develop/01.Modern React with Redux/21.asyncthunks-listofusers/src/store/index.jsx
+++ MasterReact/Udemy-ReactCourses/Udemy-React-Stephen-Develop/01.Modern React with Redux/21.asyncthunks-listofusers/src/store/index.jsx
@@ -1,7 +1,6 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { userReducer } from "./slices/userSlices";
 
-
 export const store = configureStore({
     reducer: {
         user: userReducer,
