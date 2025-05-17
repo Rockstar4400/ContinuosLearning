091017e80bd0569805ec37a82d9ddccddfc55efb
Alberto Del Angel <rockstar4400@gmail.com>
2025-05-17 16:01:36 -0600

    Master Angular - Hackerrank wheather exercise

--- MasterAngular/Practices/Hackerrank/temperature-converter/src/app/temperatureConverter/temperatureConverter.component.ts
+++ MasterAngular/Practices/Hackerrank/temperature-converter/src/app/temperatureConverter/temperatureConverter.component.ts
@@ -1,14 +1,29 @@
-import { Component, OnChanges, OnInit } from "@angular/core";
-import { input } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
+import { FormsModule } from "@angular/forms";
 @Component({
   selector: "temperature-converter",
   templateUrl: "./temperatureConverter.component.html",
   styleUrls: ["./temperatureConverter.component.scss"],
+  imports: [FormsModule]
 })
 export class TemperatureConverter implements OnInit {
   
-  celsius = input('');
+  Celsius = '';
+  Fahrenheit = '';
 
   ngOnInit() {}
+
+  c_Fahrenheit(event: Event){
+    //(°F) - 32) * 5/9
+    const c_converted = (<HTMLInputElement>event.target).valueAsNumber * 9/5 + 32;
+    
+    this.Fahrenheit = c_converted.toString();
+  }
+
+  c_Celsius(event: Event){
+    //(°C) * 9/5) + 32
+    const c_converted = (<HTMLInputElement>event.target).valueAsNumber - 32 * 5/9;
+    this.Celsius = c_converted.toString();
+  }
 
 }
--- MasterAngular/Practices/Hackerrank/weather-component/src/app/weatherDetails/weatherDetails.component.ts
+++ MasterAngular/Practices/Hackerrank/weather-component/src/app/weatherDetails/weatherDetails.component.ts
@@ -1,15 +1,43 @@
-import { CommonModule } from "@angular/common";
-import { Component, Input, OnInit } from "@angular/core";
+import { CommonModule } from '@angular/common';
+import { Component, Input, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
-  selector: "weather-details",
-  templateUrl: "./weatherDetails.component.html",
-  styleUrls: ["./weatherDetails.component.scss"],
-  imports: [CommonModule]
+  selector: 'weather-details',
+  templateUrl: './weatherDetails.component.html',
+  styleUrls: ['./weatherDetails.component.scss'],
+  imports: [CommonModule, FormsModule, CommonModule],
 })
 export class WeatherDetails implements OnInit {
+  City = '';
+  humidity = '';
+  wind = '';
+  temperature = '';
+  counter = 0;
+  @Input() weatherData: weatherData[] = [{}];
 
   ngOnInit() {}
- 
+
+  search(event: Event) {
+    
+    let filteredCity = this.weatherData.filter((city) => {
+      return (
+        city.name!.toLocaleLowerCase() ===
+        (<HTMLInputElement>event.target).value.toLocaleLowerCase()
+      );
+    });
+    this.counter = filteredCity.length;
+    if(filteredCity.length > 0){
+      this.humidity = filteredCity[0].humidity!;
+      this.wind = filteredCity[0].wind!;
+      this.temperature = filteredCity[0].temperature!;
+    }
+  }
 }
 
+interface weatherData {
+  name?: string;
+  wind?: string;
+  temperature?: string;
+  humidity?: string;
+}
