0649d067da064330bf0378e496c979456996858d
Alberto Del Angel <rockstar4400@gmail.com>
2025-06-18 20:16:04 -0600

    Master Angular - Continue with chapter 12

--- MasterAngular/Udemy-AngularCourses/12.HTTP Request New/01-starting-project/src/app/places/available-places/available-places.component.ts
+++ MasterAngular/Udemy-AngularCourses/12.HTTP Request New/01-starting-project/src/app/places/available-places/available-places.component.ts
@@ -1,9 +1,10 @@
-import { Component, inject, signal } from '@angular/core';
+import { Component, DestroyRef, inject, OnInit, signal } from '@angular/core';
 
 import { Place } from '../place.model';
 import { PlacesComponent } from '../places.component';
 import { PlacesContainerComponent } from '../places-container/places-container.component';
 import { HttpClient } from '@angular/common/http';
+import { catchError, map, throwError } from 'rxjs';
 
 @Component({
   selector: 'app-available-places',
@@ -12,9 +13,52 @@ import { HttpClient } from '@angular/common/http';
   styleUrl: './available-places.component.css',
   imports: [PlacesComponent, PlacesContainerComponent],
 })
-export class AvailablePlacesComponent {
+export class AvailablePlacesComponent implements OnInit{
   places = signal<Place[] | undefined>(undefined);
+  isFetchig = signal(false);
+  error = signal('');
   private httpClient = inject(HttpClient);
+  private destroyRef = inject(DestroyRef);
 
-  
+  ngOnInit(): void {
+    this.isFetchig.set(true);
+    const subscrition = this.httpClient
+      .get<{ places: Place[] }>('http://localhost:3000/places')
+      .pipe(
+        map((resData) => resData.places),
+        catchError((error) => {
+          console.log(error); 
+          return throwError(() => 
+            new Error('Connection Error')
+          );
+        })
+      )
+      .subscribe({
+        next: (places) => {
+          this.places.set(places);
+        },
+        error: (error: Error) => {
+          this.error.set(error.message);
+        },
+        complete: () => {
+          this.isFetchig.set(false);
+        },
+      });
+
+    this.destroyRef.onDestroy(()=>{
+      subscrition.unsubscribe();
+    })
+  }
+
+  onSelectPlace(selectedPlace: Place){
+    this.httpClient
+    .put('http://localhost:3000/user-places', 
+      {
+        placeId: selectedPlace.id
+      }).subscribe(
+        {
+          next: (resData) => console.log(resData),
+        }
+      );
+  }
 }
