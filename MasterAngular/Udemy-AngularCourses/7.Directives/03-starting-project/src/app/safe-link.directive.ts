4feb0bfdb5202051832ad8727993b5ffc7b0b336
Alberto Del Angel <rockstar4400@gmail.com>
2025-08-14 14:16:42 -0600

    Master Angular - Forms 2 ways

--- MasterAngular/Udemy-AngularCourses/7.Directives/03-starting-project/src/app/safe-link.directive.ts
+++ MasterAngular/Udemy-AngularCourses/7.Directives/03-starting-project/src/app/safe-link.directive.ts
@@ -1,4 +1,4 @@
-import { Directive } from "@angular/core";
+import { Directive, ElementRef, inject, input } from "@angular/core";
 
 @Directive({
     selector: 'a[appSafeLink]',
@@ -8,13 +8,21 @@ import { Directive } from "@angular/core";
     }
 })
 export class SafeLinkDirective {
+    queryParam = input('myapp', {alias: 'appSafeLink'});
+    private hostElementRef = 
+    inject<ElementRef<HTMLAnchorElement>>(ElementRef);
+
     constructor(){
         console.log('Directive')
     }
 
     onConfirmLeavePage (event: MouseEvent){
-        const wantsToLeave = window.confirm('Do you want to leave the app?')
+        const wantsToLeave = window.confirm('Do you want to leave the app?');
+
         if(wantsToLeave){
+            const address = this.hostElementRef.nativeElement.href;
+            this.hostElementRef.nativeElement.href = address + '?from=' +
+            this.queryParam();
             return;
         }
 
