3a35129f4054e3030b22cd648622a83ffb6a8699
Alberto Del Angel <rockstar4400@gmail.com>
2025-08-25 17:00:54 -0600

    Master Angular - Chapter 15

--- MasterAngular/Udemy-AngularCourses/a13.Forms 2 ways/src/app/auth/login/login.component.ts
+++ MasterAngular/Udemy-AngularCourses/a13.Forms 2 ways/src/app/auth/login/login.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { afterNextRender, Component, DestroyRef, inject, viewChild } from '@angular/core';
+import { FormsModule, NgForm } from '@angular/forms';
+import { debounceTime, Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-login',
@@ -8,4 +9,41 @@ import { FormsModule } from '@angular/forms';
   templateUrl: './login.component.html',
   styleUrl: './login.component.css',
 })
-export class LoginComponent {}
+export class LoginComponent {
+
+  private form = viewChild.required<NgForm>('form');
+  private destroyRef = inject(DestroyRef);
+
+  constructor (){
+    afterNextRender(() => {
+      const savedForm = window.localStorage.getItem('saved-loging-form');
+
+      if(savedForm){
+        const loadedFormData = JSON.parse(savedForm);
+        const savedEmail = loadedFormData.email;
+        setTimeout(()=> {
+        this.form().controls['email'].setValue(savedEmail);
+        }, 1)
+      }
+
+      const subscription = this.form()?.valueChanges?.pipe(debounceTime(500)).subscribe({
+        next: (value) => window.localStorage.setItem('saved-loging-form',
+          JSON.stringify({email: value.email})
+        ),
+      });
+
+      this.destroyRef.onDestroy(() => subscription?.unsubscribe());
+    });
+  }
+
+  onSubmit(formData: NgForm){
+    if(formData.form.invalid){
+      return;
+    }
+
+    const enterEmail = formData.form.value.email;
+    const enterPassword = formData.form.value.password;
+
+    formData.form.reset();
+  }
+}
