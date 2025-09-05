1c441bd3cb72b8d8525902272f589ca544749a9b
Alberto Del Angel <rockstar4400@gmail.com>
2025-09-05 14:23:48 -0600

    Master React - HR practice

--- MasterReact/HR-Practices/HackerRankCertification/02.sliceshow/src/components/Slides.js
+++ MasterReact/HR-Practices/HackerRankCertification/02.sliceshow/src/components/Slides.js
@@ -1,57 +1,55 @@
-import React from 'react'
+import React, { useState } from "react";
 
-class Slides extends React.Component {
+function Slides({ slides }) {
 
-  constructor(props){
-    super(props);
-    this.state = { slides: this.props.slides, index: 0 }
-    this.Prev = this.Prev.bind(this);
-    this.Next = this.Next.bind(this);
-    this.Restart = this.Restart.bind(this);
+  const [index, setIndex] = useState(0);
+
+  function restart(){
+    setIndex(0);
   }
 
-  Prev(){
-    this.setState({index: this.state.index - 1})
+  function next(){
+    setIndex(index + 1);
   }
 
-  Next(){
-    this.setState({index: this.state.index + 1})
+  function prev(){
+    setIndex(index - 1);
   }
 
-  Restart(){
-    this.setState({index: 0})
-  }
-  
-  renderSlides = this.props.slides.map(({title,text},id) =>{
-      return (
-        <div id='slide' className='card text-center' key={id}>
-          <h1 data-testid='title'>{title}</h1>
-          <p data-testid='text'>{text}</p>
-        </div>);
-    });
-
-  render() {
-    const len = this.props.slides.length;
-    return (
-      <div>
-        <div id='navigation' className='text-center'>
-          <button data-testid="button-restart"
-            onClick = { this.Restart }
-            disabled = { this.state.index > 0 ? false : true }
-            >Restart</button>
-          <button data-testid="button-prev"
-            disabled = { this.state.index > 0 ? false : true }
-            onClick={this.Prev}
-            >Prev</button>
-          <button data-testid="button-next"
-            disabled = { this.state.index >= len - 1  ? true : false }
-            onClick={this.Next}
-            >Next</button>
-        </div>
-        {this.renderSlides[this.state.index]}        
+  const slide = slides[index];
+  return (
+    <div>
+      <div id="navigation" className="text-center">
+        <button 
+          data-testid="button-restart"
+          disabled = { index > 0 ? false : true }
+          onClick={restart}
+          className="small outlined">
+          Restart
+        </button>
+        <button 
+          data-testid="button-prev"
+          disabled = { index > 0 ? false : true }
+          onClick={prev}
+          className="small">
+          Prev
+        </button>
+        <button 
+          data-testid="button-next"
+          disabled = { index < slides.length - 1  ? false : true }
+          onClick={next}
+          className="small">
+          Next
+        </button>
       </div>
-    )
-  }
+      { 
+      <div id="slide" className="card text-center">
+        <h1 data-testid="title">{slide.title}</h1>
+        <p data-testid="text">{slide.text}</p>
+      </div>
+      }
+    </div>
+  );
 }
 
-export default Slides
+export default Slides;
